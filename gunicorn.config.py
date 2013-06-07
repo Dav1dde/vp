77774647b8c56aaf71878a57fd698e550381162e
David <admin@dav1d.de>
2013-06-07 20:11:34 +0200

    added gunicorn config

---
+++ gunicorn.config.py
@@ -0,0 +1,11 @@
+import os.path
+
+base_path = os.path.split(os.path.abspath(__file__))[0]
+
+bind = '0.0.0.0:9655'
+workers = 2
+keepalive = 1
+user = 'vp'
+errorlog = os.path.join(base_path, 'log', 'error.log')
+loglevel = 'warning'
+proc_name = 'vp gunicorn'
